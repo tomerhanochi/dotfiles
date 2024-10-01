4eea9d024fc8c5a9b208fcff3c4d32150bd2760e
tomerhanochi <git@tomerhanochi.com>
2024-10-01 21:12:45 +0300

    Removed vi, vim and nano aliases. Changed default editor from nvim to helix

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -6,7 +6,7 @@ export XDG_CACHE_HOME="${XDG_CACHE_HOME:-"$HOME/.cache"}"
 export XDG_STATE_HOME="${XDG_STATE_HOME:-"$HOME/.local/state"}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:-"$HOME/.local/share"}"
 
-export EDITOR=nvim
+export EDITOR=hx
 export VIEWER="$EDITOR"
 
 export GPG_TTY="$(tty)"
@@ -17,14 +17,9 @@ export STARSHIP_CONFIG="$XDG_CONFIG_HOME/starship/starship.toml"
 ######## FUNCTIONS ########
 ###########################
 
-
 ###########################
 ######### ALIASES #########
 ###########################
-alias nano="$EDITOR"
-alias nvim="$EDITOR"
-alias vim="$EDITOR"
-alias vi="$EDITOR"
 
 ###########################
 ######## AUTOLOADS ########
